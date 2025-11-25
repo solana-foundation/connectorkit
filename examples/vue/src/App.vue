e16083a9de474eebd7468f7d8825e74660549c2c
Steven <sarmiento.steven@gmail.com>
2025-11-24 19:56:37 -0800

    chore: remove broken examples, change from camel to kebab for folder naming.

--- examples/vue/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="min-h-screen bg-gray-50">
-        <WalletDemo />
-    </div>
-</template>
-
-<script setup lang="ts">
-import WalletDemo from './components/WalletDemo.vue';
-</script>
